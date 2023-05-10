d16382414e5b02b28368d2c63940ac56659e0dd9
Diegov357 <Diegov_357@hotmail.com>
2023-05-10 13:49:40 -0300

    actualizado

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,1 +1,24 @@
+let botonShop = document.querySelector('.boton-shop');
+
+botonShop.addEventListener('click', ()=>{
+    location.replace('./pages/shop.html');
+})
+
+let botonShop1 = document.querySelector('.botproduc1');
+
+botonShop1.addEventListener('click', ()=>{
+    location.replace('./pages/shop.html');
+})
+
+let botonShop2 = document.querySelector('.botproduc2');
+
+botonShop2.addEventListener('click', ()=>{
+    location.replace('./pages/shop.html');
+})
+
+let botonShop3 = document.querySelector('.botproduc3');
+
+botonShop3.addEventListener('click', ()=>{
+    location.replace('./pages/shop.html');
+})
 
